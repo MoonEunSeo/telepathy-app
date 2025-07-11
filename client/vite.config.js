a035d1c1479dcfb4c3952c13fc32db477f41b3e8
노랭이 <mos8594@naver.com>
2025-07-11 18:18:03 +0900

    큰일났다13

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5179,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Express 서버 주소
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
+
+/*
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
 dotenv.config(); // 환경변수 불러오기
@@ -19,4 +38,4 @@ export default defineConfig({
       },
     },
   },
-});
+});*/
